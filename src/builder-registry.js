864107172238d05916666b3fb8d7a0f4f015cd30
Todd Morey <todd@toddmorey.com>
2025-05-21 08:42:33 -0500

    adjusted import links

--- src/builder-registry.js
+++ src/builder-registry.js
@@ -1,12 +1,35 @@
 import { Builder } from "@builder.io/react";
 import { designTokens } from './design-tokens';
+import Button from "./components/button/Button";
 
-import Counter from "./components/Counter/Counter";
+import Counter from "./components/counter/Counter";
 
 Builder.registerComponent(Counter, {
   name: "Counter",
 });
 
+Builder.registerComponent(Button, {
+  name: "Button",
+  inputs: [
+    {
+      name: "label",
+      type: "string",
+      defaultValue: "Button",
+      label: "Label",
+      required: true,
+    },
+    {
+      name: "variant",
+      type: "enum",
+      options: ["primary", "secondary"],
+      defaultValue: "primary",
+      label: "Variant",
+      required: true,
+    },
+  ],
+});
+
 Builder.register("editor.settings", {
+  styleStrictMode: true,
   designTokens: designTokens,
 });
